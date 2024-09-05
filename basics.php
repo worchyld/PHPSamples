eb46550e40b6d56d48edad66a223818cd1b549f9
AD <silentflute.ad@gmail.com>
2024-09-05 10:52:12 +0100

    Basics of php

--- basics.php
+++ basics.php
@@ -9,6 +9,10 @@ displayCoins($coins);
 print("\n");
 $coins = decrement($coins);
 displayCoins($coins);
+print("\n");
+print square(5);
+print("\n");
+print_r(small_numbers());
 
 function increment($coins) {
     return $coins += 3;
@@ -16,6 +20,13 @@ function increment($coins) {
 
 function decrement($coins) {
     return $coins -= 1;
+}
+function square($num) {
+    return $num * $num;
+}
+
+function small_numbers() {
+    return [0,1,2];
 }
 
 function displayCoins($coins) {
