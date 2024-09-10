dc0780156a2e03ee7b2e82c0d7284fc2668995b0
AD <silentflute.ad@gmail.com>
2024-09-10 13:07:58 +0100

    updating form processing page

--- express/form-processing.php
+++ express/form-processing.php
@@ -9,22 +9,27 @@ function writeContentsToFile($file, $contents) {
     file_put_contents('processing.log', 'Content recieved: ' . $contents . "\n", FILE_APPEND);
 }
 
+// set exception handler?
 set_error_handler("displayError");
 
 // force to uppercase
-$request_method = strtoupper($_SERVER['REQUEST_METHOD']);
+$request_method = mb_strtoupper($_SERVER['REQUEST_METHOD']);
 
 try {
     if ($request_method == 'POST') {
 
+        // stricter check : is it a post check, does it exist, is it a string, etc.
+        // check the fields are expected
         $blogAuthor = (string) $_POST['blogAuthor'];
         $blogTitle = (string) $_POST['blogTitle'];
         $blogContent = (string) $_POST['blogContent'];
             
         $blogAuthor = htmlspecialchars( trim($blogAuthor) , ENT_QUOTES, 'UTF-8' ); 
         $blogTitle = htmlspecialchars( trim($blogTitle) , ENT_QUOTES, 'UTF-8' );
-        $blogContent = htmlspecialchars( trim($blogContent) , ENT_QUOTES, 'UTF-8' );
-    
+        $blogContent = htmlspecialchars( trim($blogContent) , ENT_QUOTES, 'UTF-8' );    
+
+        // checks the bytes vs check the length
+        // mb_strlen: checks the # of chars
         if (str_len( $blogAuthor ) > 5000) {
             trigger_error("author: string len > 5000", E_USER_WARNING);
         }
@@ -38,7 +43,6 @@ try {
         print ("REACHED FORM PROCESSING\n");
         print ("Attempting to write to file ...");
 
-        //$contents = "{ author: " . $blogAuthor . ", title: " . $blogTitlen .", content: " . $blogContent. " }"";
         $contents = "Recieved:\n";
         $contents .= "author: ". $blogAuthor ."\n";
         $contents .= "title: ". $blogTitle ."\n";
