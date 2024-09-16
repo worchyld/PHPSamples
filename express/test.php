95bd073e26d24b1a346e2acbc6052719ec066696
AD <silentflute.ad@gmail.com>
2024-09-16 15:55:11 +0100

    updating test

--- express/test.php
+++ express/test.php
@@ -1,14 +1,14 @@
 <?php
 ini_set('session.gc_maxlifetime', 3600); // 1 hour
-//ini_set('session.save_path', $_SERVER['DOCUMENT_ROOT'] . '/sessions');
-session_set_cookie_params(3600);
-
 session_start();
 
 if (!isset($_SESSION['username'])) {
     $_SESSION['username'] = 'testy';
     echo "Session ID: " . session_id() . "<br>";
     session_write_close();
+}
+else {
+    print "<br>Found: " . $_SESSION['username'];
 }
 
 var_dump($_SESSION);
